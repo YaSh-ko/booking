d8128c77781b6c787943fbe126e4881789cdefb9
Peter <maliginpetr05@gmail.com>
2025-11-10 18:24:19 +0300

    add email autorization

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import hotelsRoutes from './routes/hotels.js';
 import bookingRotes from './routes/booking.js';
+import auth from './routes/auth.js';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use('/api/hotels', hotelsRoutes);
 
 app.use('/api/booking', bookingRotes);
+
+app.use('/api/auth', auth);
 
 const PORT = 3234
 app.listen(PORT, () => console.log(`Сервер запущен на http://localhost:${PORT}`));
