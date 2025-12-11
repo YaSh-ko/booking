c4566f4f2f489b94ba16950eff0967b75df8eaa4
Peter <maliginpetr05@gmail.com>
2025-12-11 18:26:04 +0300

    a

--- backend/controllers/favController.js
+++ backend/controllers/favController.js
@@ -104,17 +104,17 @@ export const myFavorite = async (req, res) => {
     }
 
     // Выполняем запрос
-    const { data: favorites, error } = await query;
+    const { data: fav, error } = await query;
 
     if (error) {
       console.error('Supabase error:', error);
       return res.status(400).json({ error: error.message });
     }
 
-    res.json(favorites);
+    res.json(fav);
 
-  } catch (err) {
-    console.error('Server error:', err);
-    res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+  } catch (error) {
+    console.error('Server error: ', error);
+    return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
   }
 };
